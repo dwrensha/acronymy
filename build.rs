31541920b16037294ec436e06c63d5d768a480c4
David Renshaw <dwrenshaw@gmail.com>
2014-11-19 08:39:20 -0500

    slice notation

--- build.rs
+++ build.rs
@@ -4,7 +4,7 @@ fn main() {
     let prefix = Path::new("schema");
 
     ::capnpc::compile(prefix.clone(),
-                      vec!(Path::new("schema/grain.capnp"),
-                           Path::new("schema/util.capnp"),
-                           Path::new("schema/web-session.capnp")).as_slice());
+                      &[Path::new("schema/grain.capnp"),
+                        Path::new("schema/util.capnp"),
+                        Path::new("schema/web-session.capnp")]);
 }
