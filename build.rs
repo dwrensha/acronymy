013f9ede21fce3e1886d17a1b253509ecfd2e35b
David Renshaw <dwrenshaw@gmail.com>
2015-05-31 18:19:09 -0400

    cargo update

--- build.rs
+++ build.rs
@@ -1,10 +1,7 @@
 extern crate capnpc;
 
 fn main() {
-    let prefix = ::std::path::Path::new("schema");
-
-    ::capnpc::compile(prefix,
-                      &[::std::path::Path::new("schema/grain.capnp"),
-                        ::std::path::Path::new("schema/util.capnp"),
-                        ::std::path::Path::new("schema/web-session.capnp")]).unwrap();
+    ::capnpc::compile("schema",
+                      &["schema/grain.capnp", "schema/util.capnp",
+                        "schema/web-session.capnp"]).unwrap();
 }
